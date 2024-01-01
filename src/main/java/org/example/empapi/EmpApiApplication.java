2c00c9024559fd40238c8d55daeccce68cc19fc5
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2024-01-01 14:55:29 +0700

    Initial commit

---
+++ src/main/java/org/example/empapi/EmpApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.empapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpApiApplication.class, args);
+    }
+
+}
